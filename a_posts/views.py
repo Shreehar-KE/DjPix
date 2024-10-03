a4fe6c71661f202498d106bcca4b90425151414f
Shreehar-KE <shreehar.elancheran@gmail.com>
2024-10-03 20:42:29 +0530

    add flickr webcrawler

--- a_posts/views.py
+++ a_posts/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render, redirect
 from .models import *
 from django import forms
 from django.forms import ModelForm
+from bs4 import BeautifulSoup
+import requests
 
 
 def home_view(request):
@@ -12,12 +14,13 @@ def home_view(request):
 class PostCreateForm(ModelForm):
     class Meta:
         model = Post
-        fields = '__all__'
+        fields = ['url', 'body']
         labels = {
             'body': 'Caption',
         }
         widgets = {
-            'body': forms.Textarea(attrs={'rows': 3, 'placeholder': 'Add a caption...', 'class': 'font1 text-4xl'})
+            'body': forms.Textarea(attrs={'rows': 3, 'placeholder': 'Add a caption...', 'class': 'font1 text-4xl'}),
+            'url': forms.TextInput(attrs={'placeholder': 'Add url...'}),
         }
 
 
@@ -28,7 +31,25 @@ def post_create_view(request):
         form = PostCreateForm(request.POST)
 
         if form.is_valid():
-            form.save()
+            post = form.save(commit=False)
+
+            website = requests.get(form.data['url'])
+            sourceode = BeautifulSoup(website.text, 'html.parser')
+
+            find_image = sourceode.select(
+                'meta[content^="https://live.staticflickr.com/"]')
+            image = find_image[0]['content']
+            post.image = image
+
+            find_title = sourceode.select('h1.photo-title')
+            title = find_title[0].text.strip()
+            post.title = title
+
+            find_artist = sourceode.select('a.owner-name')
+            artist = find_artist[0].text.strip()
+            post.artitst = artist
+
+            post.save()
             return redirect('home')
 
     return render(request, 'a_posts/post_create.html', {'form': form})
