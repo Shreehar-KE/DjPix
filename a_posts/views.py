6525c1a2f0d34afac06fcc1163d9086d88c7bfaf
Shreehar-KE <shreehar.elancheran@gmail.com>
2024-10-05 13:04:52 +0530

    add category feature

--- a_posts/views.py
+++ a_posts/views.py
@@ -6,9 +6,21 @@ import requests
 from .forms import *
 
 
-def home_view(request):
-    posts = Post.objects.all()
-    return render(request, 'a_posts/home.html', {'posts': posts})
+def home_view(request, tag=None):
+    if tag:
+        posts = Post.objects.filter(tags__slug=tag)
+        tag = get_object_or_404(Tag, slug=tag)
+    else:
+        posts = Post.objects.all()
+
+    categories = Tag.objects.all()
+    context = {
+        'posts': posts,
+        'tag': tag,
+        'categories': categories,
+    }
+
+    return render(request, 'a_posts/home.html', context)
 
 
 def post_create_view(request):
@@ -34,9 +46,10 @@ def post_create_view(request):
 
             find_artist = sourceode.select('a.owner-name')
             artist = find_artist[0].text.strip()
-            post.artitst = artist
+            post.artist = artist
 
             post.save()
+            form.save_m2m()
             return redirect('home')
 
     return render(request, 'a_posts/post_create.html', {'form': form})
